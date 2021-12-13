218d45155ce240bdf01a99188d3a53ad72ca5c6b
Eridiant <eridiante@gmail.com>
2021-12-13 17:02:27 +0300

    fd fix news

--- frontend/views/news/view.php
+++ frontend/views/news/view.php
@@ -10,12 +10,14 @@ $this->title = 'My Yii Application';
 ?>
 
 <div class="nws">
-    <div class="container" style="max-width: 1200px; margin-left: auto; margin-right: auto">
+    <div class="container-lg" style="max-width: 1920px; margin-left: auto; margin-right: auto">
         <div class="nws-img">
             <?php if ($model->image): ?>
                 <?= Html::img("@web/uploads/{$model->image}", ['alt' => $model->title_ru])?>
             <?php endif; ?>
         </div>
+    </div>
+    <div class="container" style="max-width: 1200px; margin-left: auto; margin-right: auto">
         <h1><?= $model->$title; ?></h1>
         <div class="nws-text"><?= $model->$code; ?></div>
         <a href="<?=Url::toRoute('/news') ?>"><?=Yii::t('frontend', 'Назад')?></a>
