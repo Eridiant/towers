510dc076f2ff92d67776416ae11bb2f6f610caab
Eridiant <eridiante@gmail.com>
2021-12-23 11:35:29 +0300

    f fix

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -38,11 +38,3 @@ $this->title = Yii::t('frontend', 'Новости');
         <div class="news-next"><?=Yii::t('frontend', 'Показать больше')?></div>
     </div>
 </div>
-<video width="320" height="240" autoplay controls>
-    <source src="%StreamURL%" type="video/mp4">
-    <object width="320" height="240" type="application/x-shockwave-flash" data="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf">
-        <param name="movie" value="http://releases.flowplayer.org/swf/flowplayer-3.2.5.swf" /> 
-        <param name="flashvars" value='config={"clip": {"url": "%StreamURL%", "autoPlay":true, "autoBuffering":true}}' /> 
-        <p><a href="%StreamURL%">view with external app</a></p> 
-    </object>
-</video>
