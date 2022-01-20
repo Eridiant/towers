17809bd6edfa2fa04c667ef3ef698bc31bf387ea
Eridiant <eridiante@gmail.com>
2022-01-20 11:47:28 +0300

    f form validation

--- frontend/views/site/contacts.php
+++ frontend/views/site/contacts.php
@@ -60,7 +60,7 @@ $this->title = Yii::t('frontend', 'Контакты');
 				<?=Yii::t('frontend', 'Введите ваши данные и мы Вам перезвоним')?>
             </p>
             <form id="form" action="/" method="post"> 
-                <input type="text" name="name" placeholder="<?=Yii::t('frontend', 'Имя')?>" required>
+                <input type="text" name="name" placeholder="<?=Yii::t('frontend', 'Имя')?>" pattern="[\u\l]+" title="<?=Yii::t('frontend', 'только буквы')?>" required>
                 <input type="text" name="phone" placeholder="<?=Yii::t('frontend', 'Телефон')?>" pattern="\+?[0-9\s\-\(\)]+" title="<?=Yii::t('frontend', 'только цифры')?>" required>
                 <input type="text" name="country" placeholder="<?=Yii::t('frontend', 'Страна')?>">
                 <div class="contacts-wrap">
