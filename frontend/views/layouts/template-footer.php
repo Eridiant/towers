95e6f4fefd8b96778fedf56f6fc7d46f5d1f6b7c
Eridiant <eridiante@gmail.com>
2022-01-25 18:52:48 +0300

    f add scripts

--- frontend/views/layouts/template-footer.php
+++ frontend/views/layouts/template-footer.php
@@ -128,6 +128,28 @@ s0.parentNode.insertBefore(s1,s0);
 <!--End of Tawk.to Script-->
 
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCabbDzORGtAU9PwXxSc4YG0fSM7YyVEPw&region=EN&language=en"></script>
+
+<!-- Event snippet for Page view conversion page -->
+<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=TUbc_Q_HLVnE8Xk9cq7z3KbuyT22FI_HKnyw2SxK0a4ScRIEwMrCcy4t6e4DvhJ6YpyPE2I37vb_t1BYK2rWE5JMTpu1B7IDwUmRK-fSx-Wk3SqQnVViE_l4SE7YjlkIqwMrqAH9dPXg21Le-j6yRewp0UbbbVrJ1Q9Cze-hcxUfSeev4C4hRhHzCIOa7cTXJsyL1EKIAaK5DiduZrAtEUOcE58-VqLzGubyu_RawYfApsxw1BK5Wu1VloOcvFQtBdwaq0F7Weet7UMjeSmaSYw_59rI0jP10AcGcGXAhZRxDE6bus2FMmwHkskdLhWL9rnC9gLazODRLNwwF_XFfXj2moLP6qPJ6M4iiOdkWQ-xYSE5O2aW-BRfkn3BQ9_97WghugSBPUAl8tpkUTuax3z6r0pfdyZU-UjyyEPmU4XevtTcQZG63Px5yD_-8k4Qr7phZk8n0Sl9HAeHYG5mIvMz9uqBltbh-9OD1yVqf_o1G03Wbq9xGyj3QkRO6UWJqv-guYKY8C3AxOirrPdDIgEi6v-GH4zoUDeTBf3K4chySiQ7zpYOHZ4jN_g3P51NGCTXGbE3AbeTkqvlW2tg41pTLUL-b7KbaU20diXx9n9yx7jitkXacrWOv9PIvDiTB7zKVHzCxsUoGhP_PUFmpnN9dISR4xRwqwX5vTirfA1r1aLFmH0YZFirMs-GF0LuVuaDUaWdNZGjwslAf3B-tM6RccQUaSbzhgehjj9eaZX_Nqfr2R6Ge72tRvFCb5-n" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly93ZWJhdHRhY2gubWFpbC55YW5kZXgubmV0L21lc3NhZ2VfcGFydF9yZWFsL0V2ZW50JTIwc25pcHBldCUyMC0lMjBQYWdlJTIwdmlldy50eHQ_bmFtZT1FdmVudCUyMHNuaXBwZXQlMjAtJTIwUGFnZSUyMHZpZXcudHh0JnNpZD1ZV1Z6WDNOcFpEcDdJbUZsYzB0bGVVbGtJam9pTVRjNElpd2lhRzFoWTB0bGVVbGtJam9pTVRjNElpd2lhWFpDWVhObE5qUWlPaUp4ZUZab1JGbFZTMXBLZDBSNU16a3JlVk5DTmt0M1BUMGlMQ0p6YVdSQ1lYTmxOalFpT2lKMk1HUnRkWGx5V1VaSFoxUkhLMHhXZEVGYWNFZHVRalJ0TmpaSWJHTktZMDgyT0daaU5sVk1OMGhVYkdOVWJqaGtTRzlIWlU0cmNuQlFZWEI0UlhoWGNsWTBaMGt5YlVnM01ERkdMMk5zZFdoRWJtZGpPQzl2VjFScmVreHVNV2xMTm5NNGFqSnBNR1J6ZUdsNlZVeDZWRGQ2YmlzclpIbEVUbTh3U1ZBNVRtOXNPRlJQUjFoNGQyMHpOM2RtUVV0RWRTOVRiR2M5UFNJc0ltaHRZV05DWVhObE5qUWlPaUp2YkVkSlpXOHlRMGxoVlhsdGFURldabGg1TmtKb1dsQm5lR2hVU1ZGa1JHeFFRM1UxY2xKRllrVlJQU0o5"/><script>
+  gtag('event', 'conversion', {'send_to': 'AW-307879312/8pVBCO7ohZMDEJC755IB'});
+</script>
+
+<!-- Event snippet for Submit lead form conversion page
+In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
+<script type="text/javascript" src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=4mT_iHPFcvz9Cemg0U85QW9IF7IU4qsMPvx7Zc2oxKwd0NKRirEisAXMYpwsdHXJxX6NrLUboGvAtxQSIq58YS5WazBgcCkC3GTEN0gAmXV1YJ1ZgE2a0ol66ETEGhAswCv7XGYcKfKsNfsOmnJfGUmR1oysfJV5D25027E7s7T1XCmoVnu9CnzAxLpFeJYn3yMEzzrWMTBo4Npv-lsBPAxPnw-BPlc27-9MRE_k4vQTSL8naQhDfGF4DBr0d_CTA0MMRB47KGDMQzjY6zhvlgy_xyWLyalZrQPq9jOAbDCI7A7g460HAaIdsJctbw6TM2QeP5kxcyp1YdvGq6J8pZC4peseHpN5OHmOJAodWeNDLWwR-Vi7kGLe4TgL1Jp7DdH-Aj7dF7moPiN2SCeWj5rrnRnCY_XRYJ_bzHbJCWXUCJScbY0NcDvPndjwcrWsN9JBO2gxsZ17fd03lPUbNPzWrJQuBXrvM-NyG_czWpda9ECVsIdWsQEwuXigxouimrB6FZx1kQoyfnuQxuxp1azLWWZPt26wYiFT-nbifF_yI3ZheAvNbfOXMVdwwI53A9I4o0FegQjRrfRjJZLCpw9c2OCnlZzrneVozcPZiKI0a2HLBVHynFiGzFq5jVT3DeY7_cmjZdVhGUUhPIhGqqNPZVK-TyH3ItWRKkuG4_U48JTwRqTXn2TlqnzePddyqmAtQmlaxRQAjqMK52BQlBOjHLYAmW10U8rYyO6i5oIsZ9ie3PzHPlwWovZ8Ngqi6h1vfQ9f3T10oe_lajhhqw" charset="UTF-8"></script><link rel="stylesheet" crossorigin="anonymous" href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly93ZWJhdHRhY2gubWFpbC55YW5kZXgubmV0L21lc3NhZ2VfcGFydF9yZWFsL0V2ZW50JTIwc25pcHBldCUyMC0lMjBTdWJtaXQlMjBsZWFkJTIwZm9ybS50eHQ_bmFtZT1FdmVudCUyMHNuaXBwZXQlMjAtJTIwU3VibWl0JTIwbGVhZCUyMGZvcm0udHh0JnNpZD1ZV1Z6WDNOcFpEcDdJbUZsYzB0bGVVbGtJam9pTVRjNElpd2lhRzFoWTB0bGVVbGtJam9pTVRjNElpd2lhWFpDWVhObE5qUWlPaUl3Y1dSNmFVaDBTamNyUTBwWVpFNXhPVGd2ZWtOblBUMGlMQ0p6YVdSQ1lYTmxOalFpT2lKMFUzbDNaMEZJTVhKVWFFOTBNRVZNVkc1SVpVRnNiVVpNTmtaTGQyNHpibEF3TWtSVlF6a3lTbXRTYlRJd1ZIVm9ZbVJSYVRWdU5FMWFUSE52T1daUFVtUXlhemhtWVZGclptdEVWbmQ2U0ZoTU5WTkthalJzYTNSellsSlhkbkp0YkV4bVZHNWxaM2hHVm5aVU9WcEljSGRGT0VSTU1FeHRhREV5UTBKdFJ6TnFWR2wzV0dWVFJYQlZORzVRZUdvNVEzVnpXRkU5UFNJc0ltaHRZV05DWVhObE5qUWlPaUpMTVV4dVRtTlZNMDByVjNOTWRuTjJSalI2ZGtOeWJsbFZURTFtUzBzcmNrbFBjRk5ZUW1welVITTBQU0o5"/><script>
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
+  };
+  gtag('event', 'conversion', {
+      'send_to': 'AW-307879312/6s-kCOa1__ICEJC755IB',
+      'event_callback': callback
+  });
+  return false;
+}
+</script>
 <!-- Swiper JS -->
 <!-- <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
--- frontend/web/js/del.js
+++ frontend/web/js/del.js
@@ -24,7 +24,7 @@ window.addEventListener('load', () => {
         e.preventDefault();
 
         let data = $(this).serializeArray();
-        console.log(data);
+        gtag_report_conversion();
         
         $.ajax({
             url: '/site/ajax',
