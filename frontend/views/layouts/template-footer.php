f71634fad4f71d644bb8b2c3f7b2fdf3eac94092
Eridiant <eridiante@gmail.com>
2022-02-01 13:39:12 +0300

    f google metrics

--- frontend/views/layouts/template-footer.php
+++ frontend/views/layouts/template-footer.php
@@ -140,7 +140,26 @@ $lg = \backend\modules\language\models\Language::find()->where(['deleted_at' => 
     </div>
 </div>
 
+Copy the code below and paste it onto every page of your website.
 
+
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WH5KNRK');</script>
+<!-- End Google Tag Manager -->
+
+
+Additionally, paste this code immediately after the opening <body> tag:
+
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WH5KNRK"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
