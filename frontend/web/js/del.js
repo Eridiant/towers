b79cec3098f72f184c7d3efb31df2c58342234d9
Eridiant <eridiante@gmail.com>
2022-02-04 23:05:13 +0300

    f ix camera

--- frontend/web/js/del.js
+++ frontend/web/js/del.js
@@ -486,7 +486,7 @@ window.addEventListener('load', () => {
         document.querySelector('.contact-cam').addEventListener('click', (e)=>{
 			e.preventDefault();
             document.querySelector('.video').classList.add('popup-show');
-            document.querySelector('.video .popup').innerHTML = '<iframe src= "https://g2.ipcamlive.com/player/player.php?alias=61c4bc8eb383a&autoplay=1" width="100%" height="100%" frameborder="0" allowfullscreen> </iframe>';
+            document.querySelector('.video .popup').innerHTML = '<iframe src= "https://g2.ipcamlive.com/player/player.php?alias=61fd869b7caba&autoplay=1" width="100%" height="100%" frameborder="0" allowfullscreen> </iframe>';
 		})
     }
 })
