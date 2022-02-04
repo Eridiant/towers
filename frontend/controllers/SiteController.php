215454055efd3d00389f20f9b54b73ffa8bd08ff
Eridiant <eridiante@gmail.com>
2022-02-04 12:18:13 +0300

    b fix mail

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -148,7 +148,7 @@ class SiteController extends Controller
             if($model->save()){
                 Yii::$app->mailer->compose()
                     // ->setTo($mail['email'])
-                    ->setTo('info@calligraphy-towers.com')
+                    ->setTo($mail['email'])
                     ->setFrom('calligraphy@calligraphy-batumi.com')
                     ->setSubject('заявка')
                     ->setTextBody('Имя: ' . $request->post("name") . ' Телефон ' . $request->post("phone") . ' Страна ' . $request->post("country") . ' Почтаы ' . $request->post("email"))
