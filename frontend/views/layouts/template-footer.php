807d4334c8de3ebaf0cd485401f2dbd36a732fbd
Eridiant <eridiante@gmail.com>
2022-02-06 17:10:27 +0300

    f fix scripts

--- frontend/views/layouts/template-footer.php
+++ frontend/views/layouts/template-footer.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\web\View;
 
 $lg = \backend\modules\language\models\Language::find()->where(['deleted_at' => null, 'key' => $currentLang])->one()->code;
 
