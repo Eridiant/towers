fc01bea07d0542433d109418da793651debf437b
Eridiant <eridiante@gmail.com>
2022-03-20 14:43:40 +0300

    fix cam

--- frontend/web/js/del.js
+++ frontend/web/js/del.js
@@ -534,7 +534,7 @@ window.addEventListener('load', () => {
         document.querySelector('.contact-cam').addEventListener('click', (e)=>{
 			e.preventDefault();
             document.querySelector('.video').classList.add('popup-show');
-            document.querySelector('.video .popup').innerHTML = '<iframe src= "https://g2.ipcamlive.com/player/player.php?alias=622ed301066ae" width="100%" height="100%" frameborder="0" allowfullscreen> </iframe>';
+            document.querySelector('.video .popup').innerHTML = '<iframe src= "https://g2.ipcamlive.com/player/player.php?alias=6237108523c61" width="100%" height="100%" frameborder="0" allowfullscreen> </iframe>';
 		})
     }
 })
