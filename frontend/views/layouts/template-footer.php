9ccc29094e641beaa13d337ab556c5ed4973c646
Eridiant <eridiante@gmail.com>
2022-07-23 13:22:35 +0300

    f fix prom

--- frontend/views/layouts/template-footer.php
+++ frontend/views/layouts/template-footer.php
@@ -90,7 +90,7 @@ $lg = \backend\modules\language\models\Language::find()->where(['deleted_at' => 
         <div class="popup-desc">
             <h2><?=Yii::t('frontend', 'Дорогие клиенты')?>!</h2>
             <p>
-                <?=Yii::t('frontend', 'Специально для вас с 01.08 по 30.10.22 действует специальное предложение на квартиры по скидке 10% В блоке Б')?>
+                <?=Yii::t('frontend', 'Не пропусти  лучшую возможность  этого лето Заполни заявку и Получи скидку  до 50$ на м2 Предложение действует до  31 Июля.')?>
             </p>
             <form id="form-stock" action="/" method="post"> 
                 <input type="hidden" name="body" value="promotion">
