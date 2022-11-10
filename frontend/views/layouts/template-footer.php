1fea82341b0bbf63af8ee9bb202db3aa3710a5bc
Eridiant <eridiante@gmail.com>
2022-11-10 15:12:09 +0300

    ch act

--- frontend/views/layouts/template-footer.php
+++ frontend/views/layouts/template-footer.php
@@ -90,10 +90,7 @@ $lg = \backend\modules\language\models\Language::find()->where(['deleted_at' => 
         <div class="popup-desc">
             <h2><?=Yii::t('frontend', 'Дорогие клиенты')?>!</h2>
             <p>
-                <?=Yii::t('frontend', 'Грандиозные скидки и эпические предложения <strong>Black Friday</strong> от Calligraphy towers.
-Не пропустите возможность стать резидентом премиального комплекса в Батуми.
-Переходите жить в дом своей мечты уже через год.
-Предложение действует до 25 ноября.')?>
+                <?=Yii::t('frontend', 'Грандиозные скидки и эпические предложения <strong>Black Friday</strong> от Calligraphy towers. Не пропустите возможность стать резидентом премиального комплекса в Батуми. Переходите жить в дом своей мечты уже через год. Предложение действует до 25 ноября.')?>
             </p>
             <form id="form-stock" action="/" method="post" onsubmit="ym(87522082,'reachGoal','forma')"> 
                 <input type="hidden" name="body" value="promotion">
