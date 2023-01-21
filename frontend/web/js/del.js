6edfa0e347df9ca3d0998fb61828009401e4647c
Eridiant <eridiante@gmail.com>
2023-01-21 21:22:29 +0300

    fj fix

--- frontend/web/js/del.js
+++ frontend/web/js/del.js
@@ -1,5 +1,5 @@
-// window.addEventListener('load', () => {
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
     
 
