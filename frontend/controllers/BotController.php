a22439c3841a601e7de76f972805e04056fdbc85
Eridiant <eridiante@gmail.com>
2023-02-01 00:05:17 +0300

    fc fix log

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -173,7 +173,7 @@ class BotController extends Controller
 
         
 
-        switch ($query->content->type_name ?? "qqqqq") {
+        switch (isset($query->content->type_name) ? $query->content->type_name : "qqqqq") {
             case 'image':
 
                 if (isset($query->content)) {
