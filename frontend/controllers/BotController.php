849d06ae1e894d81ac57b81601e20e1694820726
Eridiant <eridiante@gmail.com>
2023-02-15 22:14:25 +0300

    bp fg fix

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -345,10 +345,10 @@ class BotController extends Controller
             $this->query = TelegramQuery::find()->where('id = :id', [':id' => $text])->one();
         } else {
             if ($text === "Назад" || $text === "Назад") {
-                $query = TelegramQuery::find()->where('query = :query', [':query' => $text])->one();
+                // $query = TelegramQuery::find()->where('query = :query', [':query' => $text])->one();
 
-                $parent_id = TelegramContent::find()->where('id = :id', [':id' => $this->user->last_visited_id])->one()->parent_id;
-                $this->query = TelegramQuery::find()->where(['content_id = :content_id', 'lang = :lang'], [':content_id' => $parent_id, ':lang' => 'ru'])->one();
+                // $parent_id = TelegramContent::find()->where('id = :id', [':id' => $this->user->last_visited_id])->one()->parent_id;
+                // $this->query = TelegramQuery::find()->where(['content_id = :content_id', 'lang = :lang'], [':content_id' => $parent_id, ':lang' => 'ru'])->one();
             } else {
                 $this->query = TelegramQuery::find()->where('query = :query', [':query' => $text])->one();
             }
