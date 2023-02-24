69f49e06a7239609aac47d69b85e1004d1afdb8c
Eridiant <eridiante@gmail.com>
2023-02-24 09:40:43 +0300

    fp fix err_id

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -124,7 +124,7 @@ class BotController extends Controller
 
         if (empty($content->photo)) {
             if (empty($this->query->content->photo)) {
-                $photo = 'AgACAgQAAxkDAAIC2mPb6jnlvTf5PzSIuHOpRZGhR-e-AAJLsDEbRie9UjIzSd969FPbAQADAgADeQADLgQ';
+                $photo = 'https://calligraphy-batumi.com/tg/output_video.mp4';
             } else {
                 $photo = $this->query->content->photo;
             }
