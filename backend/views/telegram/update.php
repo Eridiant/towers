4545e03deab81900a5b5bb252ef7c3b1d55672de
Eridiant <eridiante@gmail.com>
2023-03-16 16:16:51 +0300

    bp tg fix name

--- backend/views/telegram/update.php
+++ backend/views/telegram/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var backend\models\telegram\TelegramContent $model */
 
-$this->title = 'Update Telegram Content: ' . $model->id;
+$this->title = 'Update: ' . $model->query->query ?? "";
 $this->params['breadcrumbs'][] = ['label' => 'Telegram Contents', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
