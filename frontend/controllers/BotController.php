d8bacde7949a205e026732817ff9568f9195f0a2
Eridiant <eridiante@gmail.com>
2023-04-09 12:23:48 +0300

    bp tg fix

--- frontend/controllers/BotController.php
+++ frontend/controllers/BotController.php
@@ -467,7 +467,9 @@ class BotController extends Controller
         }
 
 
-        if ($text === "/exit" && $this->canAdmin()) $this->switchAdmin(0);
+        if ($text === "/exit" && $this->canAdmin()) {
+            $this->switchAdmin(0);
+        }
 
         if ($this->isAdmin() && $this->isAdminCommand($this->update)) {
             return;
@@ -871,7 +873,9 @@ class BotController extends Controller
 
     protected function switchAdmin($flag = self::ADMINISTRATOR_STATUS)
     {
-        if (!isset($this->user->admin)) return false;
+        if (!isset($this->user->admin)) {
+            return false;
+        }
 
         try {
             $this->user->status = $flag;
