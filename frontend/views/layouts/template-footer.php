9480e24583c52383f7f6bd12497e30f177b632e0
Eridiant <eridiante@gmail.com>
2023-05-16 21:23:47 +0300

    fc fix tb bot link

--- frontend/views/layouts/template-footer.php
+++ frontend/views/layouts/template-footer.php
@@ -165,7 +165,7 @@ $lg_num = \backend\modules\language\models\Language::find()->where(['deleted_at'
     <div class="popup"></div>
 </div>
 <div class="cont-container" style="max-width: 1600px; margin-left: auto; margin-right: auto">
-    <div class="cont-wrapper cont-bot">
+    <div class="cont-bot">
         <div class="cont-inner">
             <a href="https://t.me/calligraphy_batumi_bot" class="cont-bot-link">
                 <svg width="24" height="24"><use xlink:href="images/icons.svg#telegram"></use></svg>
